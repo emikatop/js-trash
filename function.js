a82b97efdaeaba6760301defc5e21c43226dfbe1
Emiliia <emiliakalenska@gmail.com>
2026-02-04 20:08:32 +0100

    function update

--- function.js
+++ function.js
@@ -20,4 +20,38 @@ function parametersExample(name, age) {
 }
 
 //if input not recieved: 
-function userName(username = 'Not registered');
+function userName(username = 'Not registered') {
+    alert(username);
+}
+
+//return
+function sum(a, b) {
+    return a + b;
+}
+
+let mathSum = (1, 2);
+alert(sum); // 3
+
+// if 'return' aint there a function returns undefined by default.
+
+
+/* For instance, functions that start with "show" usually show something.
+Function starting with…*/
+
+showMessage();
+getAge();
+calcSum();
+createForm();
+checkPermission();
+
+/*
+"get…" – return a value,
+"calc…" – calculate something,
+"create…" – create something,
+"check…" – check something and return a boolean, etc. */
+
+/* FUNCTION NAMING
+    - Descriptive and verbal
+    - Function prefixes- exist!!!
+    - Break huge functions into smaller ones
+*/
