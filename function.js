9bf973c6b97c767dabd4107184ef0d1b221f4f4f
Emiliia <emiliakalenska@gmail.com>
2026-02-04 18:57:06 +0100

    function update

--- function.js
+++ function.js
@@ -1,15 +1,23 @@
-// local variable
-
 function showMessage() {
     let message = "Hello, I'm JavaScript!"; // local variable
 
     alert(message);
 }
+/* 'message' only exists inside 
+the showMessage. What is made 
+inside the fuction, stays 
+inside the function. */
+showMessage(); 
 
-showMessage(); // Hello, I'm JavaScript!
-
-alert(message); // Error: message is not defined
-
-// 'message' only exists insode the showMessage. What is made inside the fuction, stays inside the function. 
+alert(message); 
+ 
 
 // global variables = variables declared outside of any function
+// "it's a good practice to minimize the use of globals, and keep the variables in their functions."
+
+function parametersExample(name, age) { 
+    alert(name + 'is ' + age + 'years old');
+}
+
+//if input not recieved: 
+function userName(username = 'Not registered');
