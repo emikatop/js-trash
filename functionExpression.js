b1c25a24c3abbc81779f07500b859de48b529125
Emiliia <emiliakalenska@gmail.com>
2026-02-05 19:41:19 +0100

    16. Function Expression update

--- functionExpression.js
+++ functionExpression.js
@@ -1,11 +1,44 @@
-// Function Declaration: 
+// Function Declaration:                             // Function Expression: 
 
-function sayHello() {
-    alert('Hi');
+function sayHello() {                                let sayHello = function () {
+    alert('Hi');                                         alert('Hi');
+};                                                   }; 
+
+// Function expression - function passed as a value
+
+// Examples: declaration vs expression
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
 }
 
-// Function Expression: 
+function showOk() {
+    alert("You agreed.");
+}
 
-let sayHello = function () {
-    alert('Hi');
-};
+function showCancel() {
+    alert("You canceled the execution.");
+}
+
+// usage: functions showOk, showCancel are passed as arguments to ask
+ask("Do you agree?", showOk, showCancel);
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no(); 
+}
+
+ask(
+    "Do you agree?",
+    function () { alert("You agreed"); },
+    function () { alert("You canceled the execution"); }
+);
+
+/*
+Here, functions are declared right inside the ask(...) call. 
+They have no name, and so are called anonymous. 
+Such functions are not accessible outside of ask 
+(because they are not assigned to variables), 
+but that’s just what we want here.
+*/
